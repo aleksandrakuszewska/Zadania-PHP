9328508f75d0fd43b481c312571277a7a4e18246
Aleksandra Kuszewska <olakuszewska1@gmail.com>
2023-06-20 15:06:49 +0200

    Zadanie 1

---
+++ zad1.php
@@ -0,0 +1,22 @@
+<?php
+class Pipeline {
+    public static function make(...$functions) {
+        return function($arg) use ($functions) {
+            $result = $arg;
+            foreach ($functions as $function) {
+                $result = $function($result);
+            }
+            return $result;
+        };
+    }
+}
+
+$function = Pipeline::make(
+    function($var) { return $var * 3; },
+    function($var) { return $var + 1; },
+    function($var) { return $var / 2; }
+);
+
+$result = $function(3);
+echo $result; // Output: 5
+?>
